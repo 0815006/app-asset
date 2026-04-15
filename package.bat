dc10fa3ad642ed7f68e2fb0c9b73e657d1abdd4d
0815006 <0815006@163.com>
2026-04-15 14:50:14 +0800

    2026-04-15 创建打包源码工具

---
+++ package.bat
@@ -0,0 +1,44 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: 设置压缩包名称
+set ZIP_NAME=asset-v1-source.zip
+
+echo ==================================================
+echo   正在打包源码项目...
+echo   排除目录: java-asset-server/target, web-asset-vue/node_modules
+echo ==================================================
+
+:: 如果旧的压缩包存在，先删除
+if exist %ZIP_NAME% (
+    echo 正在删除旧的压缩包...
+    del %ZIP_NAME%
+)
+
+:: 使用 Windows 自带的 tar 命令进行打包
+:: -a: 根据后缀名自动选择压缩算法 (zip)
+:: -c: 创建新归档
+:: -v: 显示过程
+:: -f: 指定文件名
+tar -acvf %ZIP_NAME% ^
+    --exclude="java-asset-server/target" ^
+    --exclude="web-asset-vue/node_modules" ^
+    --exclude=".git" ^
+    --exclude=".roo" ^
+    --exclude=".vscode" ^
+    --exclude="%ZIP_NAME%" ^
+    *
+
+if %ERRORLEVEL% equ 0 (
+    echo.
+    echo ==================================================
+    echo   打包成功: %ZIP_NAME%
+    echo ==================================================
+) else (
+    echo.
+    echo ##################################################
+    echo   打包失败，请检查是否安装了 tar 命令或文件被占用
+    echo ##################################################
+)
+
+pause
